85b506a536c4f5bebd76de666c3ae56ebb869050
LIN Luming <s1949868@login02.ecdf.ed.ac.uk>
2020-07-23 20:11:34 +0100

    AllInOneGo.snakemake

--- Results/AllInOneGo/AllInOneGo_batch.sh
+++ Results/AllInOneGo/AllInOneGo_batch.sh
@@ -2,17 +2,17 @@
 # Grid Engine options (lines prefixed with #$)
 #$ -cwd -V                  
 #$ -l h_rt=00:30:00
-#$ -pe sharedmem 8
+#$ -pe sharedmem 4
 #$ -l h_vmem=4G
 
-#$ -t 1-1
+#$ -t 1-2
 
 # Configure modules
 . /etc/profile.d/modules.sh
 module load python/3.4.3
-module load igmm/apps/MACS2/2.1.1
 module load igmm/apps/BEDTools/2.27.1
 module load igmm/apps/meme/4.11.1
+module load igmm/apps/MACS2/2.1.1
 
 for file in $(ls /exports/eddie/scratch/s1949868/BigWig/* | head -n $SGE_TASK_ID | tail -n1); do
 	echo $file
@@ -24,6 +24,6 @@ for file in $(ls /exports/eddie/scratch/s1949868/BigWig/* | head -n $SGE_TASK_ID
 	cd ./$fileName
 
 	# run snakemake workflow
-	snakemake -s /home/s1949868/Results/AllInOneGo/AllInOneGo.snakemake ${fileName}.bdgpeakcalls.bed -j1 -p
+	snakemake -s /home/s1949868/Results/AllInOneGo/AllInOneGo.snakemake ${fileName}.PRDM9_bound_peaks.bed -j1 -p
 
 done
