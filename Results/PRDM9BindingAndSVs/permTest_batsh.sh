bcd6839ff4208a0b4a6bc52a6f9308c0a4d0ea5f
LIN Luming <s1949868@login01.ecdf.ed.ac.uk>
2020-08-01 22:47:24 +0100

    change path

--- Results/PRDM9BindingAndSVs/permTest_batsh.sh
+++ Results/PRDM9BindingAndSVs/permTest_batsh.sh
@@ -4,15 +4,20 @@
 #$ -l h_rt=00:30:00
 #$ -l h_vmem=8G
 
-#$ -t 1-299
+#$ -t 1-83
+
+# echo -e "sampleID\tnumSVBs\tnumOverlap\tpval" > permTest_SVB.txt
 
 # Configure modules
 . /etc/profile.d/modules.sh
 module load igmm/apps/R/3.6.3
 
-for sampleID in $(cut -f 1 /exports/eddie/scratch/s1949868/SNPsAndSmallINDELs/MutNumber.noZero.sorted.txt | head -n $SGE_TASK_ID | tail -n1)
-do
+for SVBFile in $(ls /exports/eddie/scratch/s1949868/CopyNumber/permTest_SVB/BRCA_CHOL_PCPG/*.masked_cnv_breakpoints.txt | head -n $SGE_TASK_ID | tail -n1); do 
+	echo $SVBFile; 
+	sampleID=${SVBFile#*BRCA_CHOL_PCPG/}; 
+	sampleID=${sampleID%.masked*};
 	echo $sampleID
-	Rscript ~/permTest.R $sampleID
+
+	Rscript /home/s1949868/MScProject/Results/PRDM9BindingAndSVs/permTest.R $sampleID
 
 done
